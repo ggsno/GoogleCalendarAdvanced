1f6961cea230c559b4e806a2ed3cfba1d1695848
ks <ogs0518@gmail.com>
2022-02-07 18:27:02 +0900

    Refactor : seperate getTaskElementDOM function

--- src/getTaskElement.js
+++ src/getTaskElement.js
@@ -1,37 +1,50 @@
 export function getTaskElementDOM(id) {
-  const $taskElement = document.createElement("li");
+  const taskDOM = createTaskDOM(id);
+  return taskDOM;
+}
+
+function createTaskDOM(id) {
+  const $container = document.createElement("li");
   const $doneButton = document.createElement("button");
   const $title = document.createElement("input");
   const $detail = document.createElement("textarea");
   const $deadline = document.createElement("input");
   const $optionButton = document.createElement("button");
-  $doneButton.innerText = "done";
-  $title.type = "text";
-  $deadline.type = "date";
-  $optionButton.innerText = "option";
-  $title.placeholder = "title"
-  $detail.placeholder = "detail";
-  if (JSON.parse(localStorage.getItem("taskList"))[id].title) {
-    $title.value = JSON.parse(localStorage.getItem("taskList"))[id].title;
-    $detail.value = JSON.parse(localStorage.getItem("taskList"))[id].detail;
-    $deadline.value = JSON.parse(localStorage.getItem("taskList"))[id].deadline;
-  }
-  
-  $taskElement.id = id;
-  $taskElement.appendChild($doneButton);
-  $taskElement.appendChild($title);
-  $taskElement.appendChild($detail);
-  $taskElement.appendChild($deadline);
-  $taskElement.appendChild($optionButton);
-  
   const taskDOM = ({
-    "container" : $taskElement,
+    "container" : $container,
     "title" : $title,
     "detail" : $detail,
     "deadline" : $deadline,
     "doneButton" :$doneButton,
     "optionButton" : $optionButton
   });
+  initTaskDOM(id, taskDOM);
+  return taskDOM
+}
 
-  return (taskDOM);
+function initTaskDOM(id, {container, title, detail, deadline, doneButton, optionButton}) {
+  doneButton.innerText = "done";
+  doneButton.classList.add('done-button');
+  title.type = "text";
+  deadline.type = "date";
+  optionButton.innerText = "option";
+  title.placeholder = "title"
+  detail.placeholder = "detail";
+  if (checkListExist(id)) {
+    title.value = JSON.parse(localStorage.getItem("taskList"))[id].title;
+    detail.value = JSON.parse(localStorage.getItem("taskList"))[id].detail;
+    deadline.value = JSON.parse(localStorage.getItem("taskList"))[id].deadline;
+  }
+  
+  container.id = id;
+  container.appendChild(doneButton);
+  container.appendChild(title);
+  container.appendChild(detail);
+  container.appendChild(deadline);
+  container.appendChild(optionButton);
+}
+
+function checkListExist(id) {
+  // It is a temporary code.
+  return JSON.parse(localStorage.getItem("taskList"))[id].title;
 }
